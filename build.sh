a33ea4f2392a76edddeac609fb3568ce9c9c1d10
Mikael Nousiainen <mikael.nousiainen@iki.fi>
2017-01-12 22:09:29 +0200

    Load JS asynchronously. Concatenate CSS. Add link to post thumbnail images.

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ set -o pipefail
 SCRIPT_FILE=`readlink -f "${0}"`
 SCRIPT_PATH=`dirname "${SCRIPT_FILE}"`
 
-sassc -m -t compressed "${SCRIPT_PATH}/src/res/scss/main.scss" "${SCRIPT_PATH}/static/res/css/main.css"
+sassc -t compressed "${SCRIPT_PATH}/src/res/scss/main.scss" "${SCRIPT_PATH}/static/res/css/main.css"
+cat "${SCRIPT_PATH}/src/res/css/"*.css >> "${SCRIPT_PATH}/static/res/css/main.css"
